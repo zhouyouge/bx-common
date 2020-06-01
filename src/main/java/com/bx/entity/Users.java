eec415caa507ed523319dae486a31b26bbc765a8
zhouyouge <619296212@qq.com>
2020-06-01 11:21:04 +0800

    yoyo删了一个age添加了一个id

--- src/main/java/com/bx/entity/Users.java
+++ src/main/java/com/bx/entity/Users.java
@@ -5,13 +5,13 @@ import java.io.Serializable;
 public class Users implements Serializable {
    private String userName;
    private  Integer password;
-private  Integer id;
+private  String id;
 
-    public Integer getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(String id) {
         this.id = id;
     }
 
