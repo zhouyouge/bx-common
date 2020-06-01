0b716c42408e12869a7c3107cefa78991f18468b
zhouyouge <57120517+zhouyouge@users.noreply.github.com>
2020-06-01 11:34:12 +0800

    Update Users.java

--- src/main/java/com/bx/entity/Users.java
+++ src/main/java/com/bx/entity/Users.java
@@ -8,7 +8,9 @@ public class Users implements Serializable {
 private  Integer id;
    private String abc;
    //我测试gitHub和本地不一样的时候的冲突第一次测试
-   //第二次测试
+   //第二次测试 两个版本全部保留
+   
+   //第三次测试 只保留一个
 
     public Integer getId() {
         return id;
