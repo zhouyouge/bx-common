e0388404941d2eb463f12db6bdfa6bc7a85bcf6b
zhouyouge <619296212@qq.com>
2020-06-01 10:59:54 +0800

    yoyo测试

--- src/main/java/com/bx/entity/Users.java
+++ src/main/java/com/bx/entity/Users.java
@@ -1,4 +1,24 @@
 package com.bx.entity;
 
-public class Users {
+import java.io.Serializable;
+
+public class Users implements Serializable {
+   private String userName;
+   private  String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
