8934dba298117033fded6c0d1aff1d1a9b835a34
zhouyouge <57120517+zhouyouge@users.noreply.github.com>
2020-06-01 11:52:00 +0800

    Update Users.java

--- src/main/java/com/bx/entity/Users.java
+++ src/main/java/com/bx/entity/Users.java
@@ -3,11 +3,7 @@ package com.bx.entity;
 import java.io.Serializable;
 
 public class Users implements Serializable {
-   private String userName;
-   private  Integer password;
-   private  String abc;
-   private  String vc;
-private String aaa;
+  
 
 
 
@@ -15,19 +11,5 @@ private String aaa;
 
 //第七次测试
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public Integer getPassword() {
-        return password;
-    }
-
-    public void setPassword(Integer password) {
-        this.password = password;
-    }
+  //1234
 }
